0d8524fe4620acb9fde4ca60435dea657dafc9cf
nirajhatagale <nirajhatagale@gmail.com>
2020-04-10 22:59:24 +0530

    l

--- DemoProj1/src/com/nt/test/B.java
+++ DemoProj1/src/com/nt/test/B.java
@@ -1,8 +1,13 @@
 package com.nt.test;
-
+ 
 public class B {
+	public void m1() {
+		
+	}
 public static void main(String[] args) {
 	System.out.println("B.main()");
 	System.out.println("hi");
+	
+	
 }
 }
